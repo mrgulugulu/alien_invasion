f708363aae103f0a510ca1e60cd2413ca7afceee
mrgulugulu <40812281+mrgulugulu@users.noreply.github.com>
2020-01-03 16:51:43 +0800

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 
 import pygame
+from pygame.sprite import Group
 
 
 from settings import Settings
@@ -18,15 +19,18 @@ def run_game():
 
     #创建一艘飞船
     ship = Ship(ai_settings, screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
 
 
 
-
+    #开始游戏主循环
     while True:
 
 
-        gf.check_events(ship)#重构代码，让其变得更加简洁
+        gf.check_events(ai_settings, screen, ship, bullets)#重构代码，让其变得更加简洁
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings():
 
         #飞船的设置
         self.ship_speed_factor = 1.5
+
+        #子弹的设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
